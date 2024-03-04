36f7ca81e73ee2bd743b05211f3eb06122d31c26
Bryan Yang <bryan.yang255@gmail.com>
2024-03-04 21:18:10 +1100

    better narrow flow for screenshots

--- src/ProjectCard.tsx
+++ src/ProjectCard.tsx
@@ -14,29 +14,25 @@ export default function ProjectCard({ project }: {
       className="
         border border-zinc-600 hover:border-zinc-400
         bg-zinc-800 p-4 text-xl
-        overflow-auto flex flex-row justify-evenly gap-4
+        overflow-auto flex md:flex-row flex-col justify-evenly gap-4
         shadow-sm shadow-zinc-700
       "
     >
       <div className="flex flex-col gap-4" >
-        <a href={project.link} target="blank">
-          <h1 className="flex flex-row gap-1">
-            <strong>{project.name}</strong>
-            <img className="w-4 invert-[.50] translate-y-0.5" src={externalLink}/>
-          </h1>
-        </a>
+        <h1 className="flex flex-row gap-1">
+          <a href={project.link} target="blank"><strong>{project.name}</strong></a>
+          <img className="w-4 invert-[.50] translate-y-0.5" src={externalLink}/>
+        </h1>
         <p>{project.info}</p>
         <a 
-          className="
-            text-blue-400 visited:text-violet-600 flex flex-row gap-1
-          "
+          className="text-blue-400 visited:text-violet-600 flex flex-row gap-1"
           href={project.sourceLink}
           target="blank">
           <p>Check out the source code here!</p>
           <img className="w-4 invert-[.50] translate-y-0.5" src={externalLink}/>
         </a>
       </div>
-      <a className="max-w-[50%] self-center" href={project.img} target="blank">
+      <a className="md:max-w-[50%] max-w-100% self-center" href={project.img} target="blank">
         <img src={project.img} alt={"A screenshot from my project: " + project.name} />
       </a>
     </div>

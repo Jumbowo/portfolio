c52a94638b3cd696432402c1b3481a3e9a3b4b50
Bryan Yang <bryan.yang255@gmail.com>
2024-03-06 22:07:33 +1100

    bg needs higher z-index to override svg weirdness

--- src/HeaderBG.tsx
+++ src/HeaderBG.tsx
@@ -10,7 +10,7 @@ export default function HeaderBG() {
         flex flex-row justify-between
         backdrop-blur-[3px]
         backdrop-brightness-[30%]
-        fixed top-0 right-0 left-0
+        fixed top-0 right-0 left-0 z-10
       "
     >
       <Header/>

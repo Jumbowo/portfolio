a5529e164e1dc2f64d987e9a7f419b7a0c21973a
Bryan Yang <bryan.yang255@gmail.com>
2024-03-06 22:52:22 +1100

    fix title card overflow

--- src/TitleCard.tsx
+++ src/TitleCard.tsx
@@ -4,7 +4,7 @@ export default function TitleCard() {
   return(
     <Link className="self-center basis-56 px-4" to="/">
       <h1 className="text-4xl font-bold">Bryan Y</h1>
-      <h2>Software Developer</h2>
+      <h2 className="overflow-hidden whitespace-nowrap">Software Developer</h2>
     </Link>
   );
 }
